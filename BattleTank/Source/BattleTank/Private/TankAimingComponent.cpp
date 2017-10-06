25822062f72730b96236bce1a0e9db7c8962086c
Jack <doongji76@gmail.com>
2017-10-06 21:06:50 +0900

    predict projectile

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -3,6 +3,7 @@
 #include "TankAimingComponent.h"
 #include "Runtime/Engine/Classes/GameFramework/Actor.h"
 #include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
@@ -39,6 +40,25 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 
 void UTankAimingComponent::AimAt(FVector WorldSpaceTarget, float LaunchSpeed)
 {
+	if (!Barrel) { return; }
+
+	auto StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
 	
-	UE_LOG(LogTemp, Warning, TEXT("LaunchSpeed : %f"), LaunchSpeed);
+	FVector OutLaunchVelocity;
+	if (UGameplayStatics::SuggestProjectileVelocity(
+		this,
+		OUT OutLaunchVelocity,
+		StartLocation,
+		WorldSpaceTarget,
+		LaunchSpeed,
+		false,
+		0.f,
+		0.f,
+		ESuggestProjVelocityTraceOption::DoNotTrace))
+	{
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		auto TankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Aiming Direction  : %s from %s"), *AimDirection.ToString(), *TankName);
+	}
+
 }
