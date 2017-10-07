8fee21f7ba7a0c662e26d30d4b10c3da197bc56b
Jack <doongji76@gmail.com>
2017-10-07 11:46:53 +0900

    BarrelTank Elevate (using Clamped)

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -83,5 +83,5 @@ void UTankAimingComponent::MoveBarrel(FVector AimDirection)
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 	UE_LOG(LogTemp, Warning, TEXT("DeltaRotator   : %s from %s"), *DeltaRotator.ToString(), *TankName);
 
-	Barrel->Elevate(5); // TODO remove magic number
+	Barrel->Elevate(1); // -1 ~ 1
 }
--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,10 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
-
+#include "Runtime/Engine/Classes/Engine/World.h"
 
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
+	
+	auto ElevateChange = RelativeSpeed * MaxDegreesPerSecond * (GetWorld()->DeltaTimeSeconds);
+	auto RawNewRotation =  RelativeRotation.Pitch + ElevateChange;
+	auto ClampedRotation = FMath::Clamp<float>(RawNewRotation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(ClampedRotation, 0, 0));
 	UE_LOG(LogTemp, Warning, TEXT("UTankBarrel::Elevate called!!"));
 }
